a1145bf6e6ed0cae04168a221f1547969cab3b64
Stephen Whitmore <stephen.whitmore@gmail.com>
2015-07-06 22:51:33 -0700

    Exposes CLI command; index in its own module.

--- index.js
+++ index.js
@@ -4,44 +4,46 @@ var Menu = require('terminal-menu')
 var ttys = require('ttys')
 var opn = require('opn')
 
-function setupMenu (items, width) {
-  var title = 'BEEP. SELECT URL, HUMAN.\n'
-  width = Math.max(width, title.length)
+module.exports = function () {
+  function setupMenu (items, width) {
+    var title = 'BEEP. SELECT URL, HUMAN.\n'
+    width = Math.max(width, title.length)
 
-  var menu = Menu({x: 1, y: 1, width: width})
-  menu.reset()
-  menu.write(title)
-  menu.write('------------------------\n')
-  for (var i in items) {
-    menu.add(items[i])
+    var menu = Menu({x: 1, y: 1, width: width})
+    menu.reset()
+    menu.write(title)
+    menu.write('------------------------\n')
+    for (var i in items) {
+      menu.add(items[i])
+    }
+
+    menu.on('select', function (url) {
+      console.dir(url)
+      menu.close()
+      opn(url)
+    })
+
+    menu.on('close', function () {
+      ttys.stdin.setRawMode(false)
+      ttys.stdin.end()
+    })
+
+    ttys.stdin.setRawMode(true)
+    ttys.stdin.pipe(menu.createStream()).pipe(process.stdout)
   }
 
-  menu.on('select', function (url) {
-    console.dir(url)
-    menu.close()
-    opn(url)
-  })
+  (function () {
+    var items = []
+    var longestLine = 0
 
-  menu.on('close', function () {
-    ttys.stdin.setRawMode(false)
-    ttys.stdin.end()
-  })
-
-  ttys.stdin.setRawMode(true)
-  ttys.stdin.pipe(menu.createStream()).pipe(process.stdout)
+    process.stdin.pipe(concat(function (text) {
+      var urls = getUrls(text.toString())
+      for (var i in urls) {
+        var url = urls[i]
+        items.push(url)
+        longestLine = Math.max(longestLine, url.length)
+      }
+      setupMenu(items, longestLine)
+    }))
+  })()
 }
-
-(function () {
-  var items = []
-  var longestLine = 0
-
-  process.stdin.pipe(concat(function (text) {
-    var urls = getUrls(text.toString())
-    for (var i in urls) {
-      var url = urls[i]
-      items.push(url)
-      longestLine = Math.max(longestLine, url.length)
-    }
-    setupMenu(items, longestLine)
-  }))
-})()
